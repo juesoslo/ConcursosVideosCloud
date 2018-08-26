cf1fd3f23649d2f39bc565d4d4f91764d3f7a0b3
Andresdavid <andres.betin@ghlhoteles.com>
2018-08-26 12:47:02 -0500

    Definición elementos base de plataforma carga de videos

---
+++ plataforma_concurso/forms.py
@@ -0,0 +1,25 @@
+from django.forms import ModelForm, ModelChoiceField
+
+from concursos.models import VideoRelacionado, ParticipanteVideo, Participante
+
+
+class ParticipanteForm(ModelForm):
+    video = ModelChoiceField(
+        queryset=VideoRelacionado.objects.all(),
+        required=False)
+
+    class Meta:
+        model = Participante
+        fields = ('nombre', 'apellido', 'email', 'mensaje')
+
+    def __init__(self, *args, **kwargs):
+        super(ParticipanteForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['video'].initial = VideoRelacionado.objects.get(participantevideo__participante=self.instance).values_list('id', flat=True)
+
+    def save(self, commit=True):
+        super(ParticipanteForm, self).save(commit)
+        video = self.cleaned_data.pop('video', None)
+        if video:
+            self.instance.surveyimage.delete()
+            ParticipanteVideo.objects.create(participante=self.instance, video=video)
