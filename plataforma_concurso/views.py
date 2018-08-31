11a3409e4f631ebcf7686305f8e3d3290c4ba46a
Andres David Betin <andres.betin@ghlhoteles.com>
2018-08-31 15:50:16 -0500

    Adaptación plantilla videos a proyecto

--- plataforma_concurso/views.py
+++ plataforma_concurso/views.py
@@ -7,6 +7,9 @@ from plataforma_concurso.forms import ParticipanteForm
 def index(request):
     return render(request, 'detalle_concurso.html')
 
+def videos(request):
+    return render(request, 'videos_concurso.html')
+
 def formulario_participante(request):
     form = ParticipanteForm()
     return render(request, 'formulario_participante.html', {'form': form})
