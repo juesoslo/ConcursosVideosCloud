9df15c2b50cf57fefc4331cfefced8edbc619c09
FranciscoAriza <franciscoariza724@gmail.com>
2018-09-03 22:52:51 -0500

    Working with videos
    Update for work in other place

--- eventos/admin.py
+++ eventos/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from concursos.models import Concurso, VideoRelacionado, Participante, ParticipanteVideo
 
 # Register your models here.
-admin.site.register(Concurso)
-admin.site.register(VideoRelacionado)
-admin.site.register(Participante)
-admin.site.register(ParticipanteVideo)
