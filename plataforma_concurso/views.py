9e7b7bc82ca302a8deaa60047efb75cf5bbc89e9
FranciscoAriza <franciscoariza724@gmail.com>
2018-09-05 23:17:32 -0500

    Lista de Vídeos de los concursos (sitio usuarios)
    Se muestran los vídeos en estado procesado para cada uno de los concursos

--- plataforma_concurso/views.py
+++ plataforma_concurso/views.py
@@ -5,7 +5,7 @@ import json
 
 
 # Create your views here.
-from concursos.models import Concurso, VideoRelacionado, Participante, ParticipanteVideo
+from concursos.models import Concurso, VideoRelacionado, Participante, ParticipanteVideo, EstadosVideoOpciones
 from plataforma_concurso.forms import ParticipanteForm
 
 
@@ -47,17 +47,31 @@ def concurso_videos(request, idconcurso):
 
     concursos = Participante.objects.filter(concurso__url = url_concurso )
 
+    temp_conc = Concurso.objects.filter(url=idconcurso)
+
+    nombre_concurso = temp_conc[0].nombre
+
     for x in concursos:
         video = ParticipanteVideo.objects.filter(participante_id = x.id)
         print(video)
         for vid in video:
-            conv = VideoRelacionado.objects.filter(id = vid.id)
+            conv = VideoRelacionado.objects.filter(id = vid.id, estado ="Procesado")
             for t in conv:
                 temp ={}
-                part = x.id
-                vid = t.video_convertido
-                temp['part'] = part
-                temp['vid'] = vid
+                partic_id = x.id
+                partic_nombre = x.nombre
+                partic_apellido = x.apellido
+                partic_email = x.email
+                partic_mensaje = x.mensaje
+                video_original = t.video
+                video_convertido = t.video_convertido
+                temp['partic_id'] = partic_id
+                temp['partic_nombre'] = partic_nombre
+                temp['partic_apellido'] = partic_apellido
+                temp['partic_email'] = partic_email
+                temp['partic_mensaje'] = partic_mensaje
+                temp['video_original'] = video_original
+                temp['video_convertido'] = video_convertido
                 videos.append(temp)
                 print("video: ",t.video)
 
@@ -67,6 +81,7 @@ def concurso_videos(request, idconcurso):
     context = {
         "my_url": my_url,
         "url_concurso": url_concurso,
+        "concurso": nombre_concurso,
         "participantes": concursos,
         "videos": videos
 
@@ -77,7 +92,7 @@ def concurso_videos(request, idconcurso):
         return render(request, 'videos_concurso.html', context)
     else:
         print("concurso NO encontrado", url_concurso)
-        return render(request, 'error_concurso.html', context)
+        return render(request, 'videos_concurso.html', context)
 
 def concurso_participante(request, idconcurso):
     my_url = reverse(concurso, args=(idconcurso,))
