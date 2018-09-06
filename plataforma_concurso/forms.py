1845efa718de83bc58c16a2a94a970a05111ddef
juesoslo <je.osoriol@uniandes.edu.co>
2018-09-06 16:30:14 -0500

    aceptar formatos de video

--- plataforma_concurso/forms.py
+++ plataforma_concurso/forms.py
@@ -8,7 +8,8 @@ class ParticipanteForm(ModelForm):
     #     queryset=VideoRelacionado.objects.all(),
     #     required=False)
 
-    video = FileField(widget=FileInput(attrs={'accept': 'video/*'}))
+    #video = FileField(widget=FileInput(attrs={'accept': 'video/*'}))
+    video = FileField(widget=FileInput(attrs={'accept': 'video/x-flv, video/mp4, application/x-mpegURL, video/MP2T, video/3gpp, video/quicktime, video/x-msvideo, video/x-ms-wmv, .flv, video/*'}))
 
     class Meta:
         model = Participante
