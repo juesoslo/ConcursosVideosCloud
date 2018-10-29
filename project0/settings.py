b30db95a4f34ff74eb56e05c363a3565ec85360c
Andres Betin <ad.betin@uniandes.edu.co>
2018-10-28 23:31:47 -0500

    recepcion de mensajes de sqs

--- project0/settings.py
+++ project0/settings.py
@@ -192,7 +192,7 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_DEFAULT_QUEUE = 'cloudg7-videos-queue'
-# CELERY_RESULT_BACKEND = None # Disabling the results backend
+CELERY_RESULT_BACKEND = None # Disabling the results backend
 BROKER_TRANSPORT_OPTIONS = {
     'region': 'us-west-2',
     'polling_interval': 20,
