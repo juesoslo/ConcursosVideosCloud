e659c91f49bbfbc040d4576d96894876f81a5ad8
Andres David Betin <andres.betin@ghlhoteles.com>
2018-10-30 16:25:00 -0500

    Cambio por falta de librería pycurl

--- proceso_conversion/tasks.py
+++ proceso_conversion/tasks.py
@@ -1,65 +1,60 @@
 import requests
 from datetime import datetime
-from celery import shared_task
 import boto3
 import os
 
-@shared_task
-def process_message():
 
-    AWS_REGION = "us-east-2"
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", '')
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", '')
+AWS_REGION = "us-east-2"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", 'AKIAJAGAZNIQCXEFY2NQ')
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", '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')
 
-    # Create SQS client
-    sqs = boto3.client(
-        'sqs',
-        region_name=AWS_REGION,
-        # Hard coded strings as credentials, not recommended.
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-    )
+# Create SQS client
+sqs = boto3.client(
+    'sqs',
+    region_name=AWS_REGION,
+    # Hard coded strings as credentials, not recommended.
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
 
-    queue_url = 'https://sqs.us-east-2.amazonaws.com/687383508727/cloudg7-videos-queue'
+queue_url = 'https://sqs.us-east-2.amazonaws.com/687383508727/cloudg7-videos-queue'
 
-    # Receive message from SQS queue
-    response = sqs.receive_message(
-        QueueUrl=queue_url,
-        AttributeNames=[
-            'SentTimestamp'
-        ],
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=[
-            'All'
-        ],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=0
-    )
+# Receive message from SQS queue
+response = sqs.receive_message(
+    QueueUrl=queue_url,
+    AttributeNames=[
+        'SentTimestamp'
+    ],
+    MaxNumberOfMessages=1,
+    MessageAttributeNames=[
+        'All'
+    ],
+    VisibilityTimeout=0,
+    WaitTimeSeconds=0
+)
+print(response)
+message = response['Messages'][0]
+receipt_handle = message['ReceiptHandle']
 
-    message = response['Messages'][0]
-    receipt_handle = message['ReceiptHandle']
+# se ejecutan las acciones de procesar video
 
-    # se ejecutan las acciones de procesar video
+# URL final del aplicativo
+WEB_URL = os.environ.get("CLOUDG7_WEB_URL", '')
 
-    # URL final del aplicativo
-    WEB_URL = os.environ.get("CLOUDG7_WEB_URL", '')
+# URL del servicio REST que se va a ejecutar
+url = WEB_URL+'/conversion/procesar/'
 
-    # URL del servicio REST que se va a ejecutar
-    url = WEB_URL+'/conversion/procesar/'
+# Se ejecuta el servicio
+response = requests.get(url) #, json=data)
 
-    # Se ejecuta el servicio
-    response = requests.get(url) #, json=data)
+# Se imprime el resultado
+print(response.json())
 
-    # Se imprime el resultado
-    print(response.json())
+# Delete received message from queue
+sqs.delete_message(
+    QueueUrl=queue_url,
+    ReceiptHandle=receipt_handle
+)
 
-    # Delete received message from queue
-    sqs.delete_message(
-        QueueUrl=queue_url,
-        ReceiptHandle=receipt_handle
-    )
-
-    print('Received and deleted message: %s' % message)
-    print("The time is %s :" % str(datetime.now()))
-
-    return True
+print('Received and deleted message: %s' % message)
+print("The time is %s :" % str(datetime.now()))
