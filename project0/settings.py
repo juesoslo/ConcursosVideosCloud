0155f7c36682c20b61869ee30f0943c4bdcf508f
Andres Betin <ad.betin@uniandes.edu.co>
2018-11-05 15:35:24 -0500

    Correccion secret key

--- project0/settings.py
+++ project0/settings.py
@@ -174,7 +174,7 @@ WEB_URL = os.environ.get("CLOUDG7_WEB_URL", '')
 
 # AWS Credentials
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", '')
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY_ID", '')
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", '')
 AWS_DEFAULT_REGION = os.environ.get("AWS_DEFAULT_REGION", '')
 
 # AWS storage configuration
